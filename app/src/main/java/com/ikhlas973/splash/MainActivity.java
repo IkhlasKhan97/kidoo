eac6cd03a0e3373b4a6e07ae457dcd988ce81570
ikhlask <ikhlask@gmail.com>
2021-07-13 00:48:26 -0800

    Initial commit

---
+++ app/src/main/java/com/ikhlas973/splash/MainActivity.java
@@ -0,0 +1,54 @@
+package com.ikhlas973.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static int SPLASH_SCREEN  = 5000;
+
+    Animation topAnim , bottomAnim;
+    ImageView image;
+    TextView logo;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+            //Animations
+        topAnim = AnimationUtils.loadAnimation(this , R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this , R.anim.bottom_animation);
+
+             //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
+    }
+
+}
